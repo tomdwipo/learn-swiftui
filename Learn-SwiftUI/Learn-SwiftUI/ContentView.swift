ce71874d026ed6b2faf192d315780d1fd7ff635c
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2021-11-30 22:32:54 +0700

    chore: add first project

---
+++ Learn-SwiftUI/Learn-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learn-SwiftUI
+//
+//  Created by Tommy on 30/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learn-SwiftUI/Learn-SwiftUI/Learn_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_SwiftUIApp.swift
+//  Learn-SwiftUI
+//
+//  Created by Tommy on 30/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Learn_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
