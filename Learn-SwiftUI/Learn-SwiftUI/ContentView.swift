3a64be11ee241e5b6254c40ce851b4f2af5053a4
Tommy Dwi Putranto <tommyputranto1@gmail.com>
2021-11-30 22:47:41 +0700

    refactor: tests update

--- Learn-SwiftUI/Learn-SwiftUI/ContentView.swift
+++ Learn-SwiftUI/Learn-SwiftUI/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hello World!").padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.portrait)
     }
 }
